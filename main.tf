1b1cbef423ffce0f2b047044ebc9a00b1ba136db
Jonathan Porta <jonathan@jonathanporta.com>
2023-04-27 23:14:23 -0600

    Cleanup

--- main.tf
+++ main.tf
@@ -62,29 +62,6 @@ resource "aws_s3_bucket_website_configuration" "app_bucket_website" {
       }
     }
   }
-  # routing_rule {
-  #   condition {
-  #     key_prefix_equals = var.routing_rule_key_prefix_equals
-  #   }
-  #   redirect {
-  #     replace_key_with = var.routing_rule_replace_key_with
-  #   }
-  # }
-  # routing_rules = var.routing_rule
-  # dynamic "routing_rule" {
-  #   for_each = var.routing_rule
-  #   condition {
-  #     http_error_code_returned_results = routing_rule.value.Condition.HttpErrorCodeReturnedEquals
-  #     key_prefix_equals                = routing_rule.value.Condition.KeyPrefixEquals
-  #   }
-  #   redirect {
-  #     host_name               = routing_rule.value.Redirect.HostName
-  #     http_redirect_code      = routing_rule.value.Redirect.HttpRedirectCode
-  #     protocol                = routing_rule.value.Redirect.Protocol
-  #     replace_key_prefix_with = routing_rule.value.Redirect.ReplaceKeyPrefixWith
-  #     replace_key_with        = routing_rule.value.Redirect.ReplaceKeyWith
-  #   }
-  # }
 }
 
 locals {
--- variables.tf
+++ variables.tf
@@ -10,44 +10,21 @@ variable "source_files" {
   description = "A path to the website's source files. These will be uploaded to the bucket."
   type        = string
 }
-
-# variable "routing_rule" {
-#   description = "An optional list of routing rules to apply to the bucket."
-#   type        = string
-#   # list(object({
-#   #   Condition = object({
-#   #     HttpErrorCodeReturnedEquals = string
-#   #     KeyPrefixEquals             = string
-#   #   })
-#   #   Redirect = object({
-#   #     HostName             = string
-#   #     HttpRedirectCode     = string
-#   #     Protocol             = string
-#   #     ReplaceKeyPrefixWith = string
-#   #     ReplaceKeyWith       = string
-#   #   })
-#   # }))
-#   default = ""
-# }
-
 variable "routing_rule_key_prefix_equals" {
   description = "The optional key prefix to match."
   type        = string
   default     = null
 }
-
 variable "routing_rule_replace_key_with" {
   description = "The optional name key to replace with."
   type        = string
   default     = null
 }
-
 variable "index_document_suffix" {
   description = "The optional name of the index document to use for the bucket."
   type        = string
   default     = "index.html"
 }
-
 variable "error_document_key" {
   description = "The optional name of the error document to use for the bucket."
   type        = string

b9c19f31d9a586a36889de6d1f4cf99818bdfdc6
Jonathan Porta <jonathan@jonathanporta.com>
2023-04-27 23:14:23 -0600

    Add betteruptime for monitoring.

--- main.tf
+++ main.tf
@@ -62,29 +62,6 @@ resource "aws_s3_bucket_website_configuration" "app_bucket_website" {
       }
     }
   }
-  # routing_rule {
-  #   condition {
-  #     key_prefix_equals = var.routing_rule_key_prefix_equals
-  #   }
-  #   redirect {
-  #     replace_key_with = var.routing_rule_replace_key_with
-  #   }
-  # }
-  # routing_rules = var.routing_rule
-  # dynamic "routing_rule" {
-  #   for_each = var.routing_rule
-  #   condition {
-  #     http_error_code_returned_results = routing_rule.value.Condition.HttpErrorCodeReturnedEquals
-  #     key_prefix_equals                = routing_rule.value.Condition.KeyPrefixEquals
-  #   }
-  #   redirect {
-  #     host_name               = routing_rule.value.Redirect.HostName
-  #     http_redirect_code      = routing_rule.value.Redirect.HttpRedirectCode
-  #     protocol                = routing_rule.value.Redirect.Protocol
-  #     replace_key_prefix_with = routing_rule.value.Redirect.ReplaceKeyPrefixWith
-  #     replace_key_with        = routing_rule.value.Redirect.ReplaceKeyWith
-  #   }
-  # }
 }
 
 locals {
@@ -99,4 +76,12 @@ resource "aws_s3_object" "app_bucket_source" {
   etag         = filemd5("${var.source_files}/${each.value}")
   acl          = "public-read"
   content_type = lookup(local.mime_types, regex("\\.[^.]+$", each.value), null)
+}
+
+resource "betteruptime_monitor" "this" {
+  url               = "https://${var.hostname}"
+  monitor_type      = "status"
+  domain_expiration = 30
+  follow_redirects  = true
+  ssl_expiration    = 30
 }
--- providers.tf
+++ providers.tf
@@ -4,5 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.8.0"
     }
+    betteruptime = {
+      # https://github.com/BetterStackHQ/terraform-provider-better-uptime/blob/master/CHANGELOG.md
+      source  = "BetterStackHQ/better-uptime"
+      version = "~> 0.3.15"
+    }
   }
 }
